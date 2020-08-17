1b569ddf4d89ef91ab10390d1ecb682dce64e901
unknown <vishalpwaman123@gmail.com>
2020-08-17 17:41:02 +0530

    Added : MSMQ Feature In UnPark API.

--- ParkingLotAPI/Controllers/ParkingController.cs
+++ ParkingLotAPI/Controllers/ParkingController.cs
@@ -10,6 +10,7 @@ using CommonLayer.ResponseModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 using ParkingLotAPI.MSMQSender;
 
 namespace ParkingLotApi.Controllers
@@ -20,14 +21,17 @@ namespace ParkingLotApi.Controllers
     {
         private IParkingBL parkingLotBL;
 
+        private readonly IDistributedCache distributedCache;
+
         /// <summary>
         /// Declare Sender object
         /// </summary>
         Sender senderObject = new Sender();
 
-        public ParkingController(IParkingBL parkingLotBL)
+        public ParkingController(IParkingBL parkingLotBL, IDistributedCache distributedCache)
         {
             this.parkingLotBL = parkingLotBL;
+            this.distributedCache = distributedCache;
         }
 
         //[Authorize(Roles = "Admin, Driver, Attendant ,Owner")]
@@ -37,6 +41,11 @@ namespace ParkingLotApi.Controllers
         {
             try
             {
+
+                string cacheKey = "Parking";
+
+                distributedCache.Remove(cacheKey);
+
 
                 if (parkingDetails.VehicalOwnerName == null || parkingDetails.VehicalNumber == null || parkingDetails.VehicalOwnerEmail == null ||
                      parkingDetails.Brand == null || parkingDetails.Color == null || parkingDetails.DriverName == null)
@@ -192,23 +201,23 @@ namespace ParkingLotApi.Controllers
         [Route("Slot/Availability")]
         public IActionResult CheckLotStatus()
         {
-        try
-        {
-        bool status = this.parkingLotBL.CheckLotStatus();
+            try
+            {
+                    bool status = this.parkingLotBL.CheckLotStatus();
 
-        if (status)
-        {
-        return Ok(new { Success = true, Message = "Lot Is Available" });
-        }
-        else
-        {
-        return NotFound(new { Success = false, Message = "Lot Is Full" });
-        }
-        }
-        catch (Exception exception)
-        {
-        return BadRequest(new { Success = false, Message = exception.Message });
-        }
+                if (status)
+                {
+                        return Ok(new { Success = true, Message = "Lot Is Available" });
+                }
+                else
+                {
+                        return NotFound(new { Success = false, Message = "Lot Is Full" });
+                }
+            }
+            catch (Exception exception)
+            {
+                 return BadRequest(new { Success = false, Message = exception.Message });
+            }
         }
 
         [Authorize(Roles = "Admin, Driver ,Owner")]
@@ -216,33 +225,33 @@ namespace ParkingLotApi.Controllers
         [Route("Vehical/{VehicalNumber}")]
         public IActionResult GetVehicalByNumber([FromRoute] string VehicalNumber)
         {
-        try
-        {
-        //Throws Custom Exception If VehicalNumber Is Not in Valid Format.
-        if (!Regex.IsMatch(VehicalNumber, @"^[A-Z]{2}\s[0-9]{2}\s[A-Z]{1,2}\s[0-9]{4}$"))
-        {
-        throw new Exception(ParkingLotExceptions.ExceptionType.INVALID_VEHICAL_NUMBER.ToString() + " Please Enter Vehical In 'MH 01 AZ 2005' This Format.");
-        }
+            try
+            {
+            //Throws Custom Exception If VehicalNumber Is Not in Valid Format.
+                if (!Regex.IsMatch(VehicalNumber, @"^[A-Z]{2}\s[0-9]{2}\s[A-Z]{1,2}\s[0-9]{4}$"))
+                {
+                        throw new Exception(ParkingLotExceptions.ExceptionType.INVALID_VEHICAL_NUMBER.ToString() + " Please Enter Vehical In 'MH 01 AZ 2005' This Format.");
+                }
 
-        var parkResponse = parkingLotBL.GetVehicalByNumber(VehicalNumber);
+                    var parkResponse = parkingLotBL.GetVehicalByNumber(VehicalNumber);
 
-        if (parkResponse != null)
-        {
-        bool Success = true;
-        var Message = "Vehical Available";
-        return Ok(new { Success, Message, Data = parkResponse });
-        }
-        else 
-        {
-        bool Success = false;
-        var Message = "Vehical Not Available";
-        return NotFound(new { Success, Message });
-        }
-        }
-        catch (Exception exception)
-        {
-        return BadRequest(new { Success = false, Message = exception.Message });
-        }
+                if (parkResponse != null)
+                {
+                        bool Success = true;
+                        var Message = "Vehical Available";
+                        return Ok(new { Success, Message, Data = parkResponse });
+                }
+                else 
+                {
+                        bool Success = false;
+                        var Message = "Vehical Not Available";
+                        return NotFound(new { Success, Message });
+                }
+            }
+            catch (Exception exception)
+            {
+                    return BadRequest(new { Success = false, Message = exception.Message });
+            }
         }
 
         [Authorize(Roles = "Admin, Police, Security, Owner")]
@@ -250,102 +259,102 @@ namespace ParkingLotApi.Controllers
         [Route("Vehicals/{Color}")]
         public IActionResult GetVehicalDetailsByColor([FromRoute] string Color)
         {
-        try
-        {
-        if (!Regex.IsMatch(Color, @"^[A-Z][a-zA-Z]*$"))
-        {
-        throw new Exception(ParkingLotExceptions.ExceptionType.INVALID_COLOR.ToString() + " Please Enter Vehical Color.");
-        }
+            try
+            {
+                if (!Regex.IsMatch(Color, @"^[A-Z][a-zA-Z]*$"))
+                {
+                        throw new Exception(ParkingLotExceptions.ExceptionType.INVALID_COLOR.ToString() + " Please Enter Vehical Color.");
+                }
 
-        var parkResponse = parkingLotBL.GetVehicalDetailsByColor(Color);
+                    var parkResponse = parkingLotBL.GetVehicalDetailsByColor(Color);
 
-        if (parkResponse != null)
-        {
-        bool Success = true;
-        var Message = "Vehical Color Available";
-        return Ok(new { Success, Message, Data = parkResponse });
-        }
-        else 
-        {
-        bool Success = false;
-        var Message = "Vehical Color Not Available";
-        return NotFound(new { Success, Message });
-        }
+                if (parkResponse != null)
+                {
+                        bool Success = true;
+                        var Message = "Vehical Color Available";
+                        return Ok(new { Success, Message, Data = parkResponse });
+                }
+                else 
+                {
+                        bool Success = false;
+                        var Message = "Vehical Color Not Available";
+                        return NotFound(new { Success, Message });
+                }
 
 
-        }
-        catch (Exception exception)
-        {
-        return BadRequest(new { Success = false, Message = exception.Message });
-        }
+            }
+            catch (Exception exception)
+            {
+                 return BadRequest(new { Success = false, Message = exception.Message });
+            }
         }
 
         [Authorize(Roles = "Admin, Police, Security, Owner ")]
         [HttpGet("{Brand}")]
         public IActionResult GetVehicalDetailsByBrand([FromRoute] string Brand)
         {
-        try
-        {
-        //Throws Custom Exception If VehicalNumber Is Not in Valid Format.
-        if (!Regex.IsMatch(Brand, @"^[A-Z][a-zA-Z]*$"))
-        {
-        throw new Exception(ParkingLotExceptions.ExceptionType.INVALID_COLOR.ToString() + " Please Enter Vehical Color.");
-        }
+            try
+            {
+            //Throws Custom Exception If VehicalNumber Is Not in Valid Format.
+                if (!Regex.IsMatch(Brand, @"^[A-Z][a-zA-Z]*$"))
+                {
+                        throw new Exception(ParkingLotExceptions.ExceptionType.INVALID_COLOR.ToString() + " Please Enter Vehical Color.");
+                }
 
-        var parkResponse = parkingLotBL.GetVehicalDetailsByBrand(Brand);
+                 var parkResponse = parkingLotBL.GetVehicalDetailsByBrand(Brand);
+        
+                if (parkResponse != null)
+                {
+                        bool Success = true;
+                        var Message = "Vehical Brand Available";
+                        return Ok(new { Success, Message, Data = parkResponse });
+                }
+                else 
+                {
+                        bool Success = false;
+                        var Message = "Vehical Brand Not Available";
+                        return NotFound(new { Success, Message });
+                }
 
-        if (parkResponse != null)
-        {
-        bool Success = true;
-        var Message = "Vehical Brand Available";
-        return Ok(new { Success, Message, Data = parkResponse });
-        }
-        else 
-        {
-        bool Success = false;
-        var Message = "Vehical Brand Not Available";
-        return NotFound(new { Success, Message });
-        }
-
-        }
-        catch (Exception exception)
-        {
-        return BadRequest(new { Success = false, Message = exception.Message });
-        }
+            }
+            catch (Exception exception)
+            {
+            return BadRequest(new { Success = false, Message = exception.Message });
+            }
         }
 
         [Authorize(Roles = "Admin, Police, Security ,Owner")]
         [HttpGet("{Brand}/{Color}")]
         public IActionResult GetVehicalDetailsByBrandAndColor([FromRoute] string Brand , [FromRoute] string Color)
         {
-        try
-        {
+            try
+            {
 
-        //Throws Custom Exception If VehicalNumber Is Not in Valid Format.
-        if (!Regex.IsMatch(Brand, @"^[A-Z][a-zA-Z]*$"))
-        {
-        throw new Exception(ParkingLotExceptions.ExceptionType.INVALID_COLOR.ToString() + " Please Enter Valied Vehical Color.");
-        }
+            //Throws Custom Exception If VehicalNumber Is Not in Valid Format.
+                if (!Regex.IsMatch(Brand, @"^[A-Z][a-zA-Z]*$"))
+                {
+                        throw new Exception(ParkingLotExceptions.ExceptionType.INVALID_COLOR.ToString() + " Please Enter Valied Vehical Color.");
+                }
 
-        var parkResponse = parkingLotBL.GetVehicalDetailsByBrandAndColor(Brand, Color);
+                    var parkResponse = parkingLotBL.GetVehicalDetailsByBrandAndColor(Brand, Color);
 
-        if (parkResponse != null)
-        {
-        bool Success = true;
-        var Message = "Vehical Brand And Color Available";
-        return Ok(new { Success, Message, Data = parkResponse });
-        }
-        else 
-        {
-        bool Success = false;
-        var Message = "Vehical Brand And Color Not Available";
-        return NotFound(new { Success, Message });
-        }
-        }
-        catch (Exception exception)
-        {
-        return BadRequest(new { Success = false, Message = exception.Message });
-        }
+                if (parkResponse != null)
+                {
+                        bool Success = true;
+                        var Message = "Vehical Brand And Color Available";
+                        return Ok(new { Success, Message, Data = parkResponse });
+                }
+                else 
+                {
+                        bool Success = false;
+                        var Message = "Vehical Brand And Color Not Available";
+                        return NotFound(new { Success, Message });
+                }
+            }
+            catch (Exception exception)
+            {
+                    return BadRequest(new { Success = false, Message = exception.Message });
+            }
         }
 
         [Authorize(Roles = "Admin, Police, Security ,Owner")]
@@ -353,34 +362,34 @@ namespace ParkingLotApi.Controllers
         [Route("vehicle/{ParkingSlot}")]
         public IActionResult GetVehicalDetailsByParkingSlot([FromRoute] string ParkingSlot)
         {
-        try
-        {
+            try
+            {
 
-        //Throws Custom Exception If VehicalNumber Is Not in Valid Format.
-        if (!Regex.IsMatch(ParkingSlot, @"^(?:A|B|C|D)$"))
-        {
-        throw new Exception(ParkingLotExceptions.ExceptionType.INVALID_PARKING_SLOT.ToString() + " Please Enter valid slot Number eg. A,B,C,D.");
-        }
+                //Throws Custom Exception If VehicalNumber Is Not in Valid Format.
+                if (!Regex.IsMatch(ParkingSlot, @"^(?:A|B|C|D)$"))
+                {
+                        throw new Exception(ParkingLotExceptions.ExceptionType.INVALID_PARKING_SLOT.ToString() + " Please Enter valid slot Number eg. A,B,C,D.");
+                }
 
-        var parkResponse = parkingLotBL.GetVehicalDetailsByParkingSlot(ParkingSlot);
+                    var parkResponse = parkingLotBL.GetVehicalDetailsByParkingSlot(ParkingSlot);
 
-        if (parkResponse != null)
-        {
-        bool Success = true;
-        var Message = " Vahicals Found In a Slot ";
-        return Ok(new { Success, Message, Data = parkResponse });
-        }
-        else 
-        {
-        bool Success = false;
-        var Message = " Vahicals Not Found In a Slot ";
-        return NotFound(new { Success, Message });
-        }
-        }
-        catch (Exception exception)
-        {
-        return BadRequest(new { Success = false, Message = exception.Message });
-        }
+                if (parkResponse != null)
+                {
+                        bool Success = true;
+                        var Message = " Vahicals Found In a Slot ";
+                        return Ok(new { Success, Message, Data = parkResponse });
+                }
+                else 
+                {
+                        bool Success = false;
+                        var Message = " Vahicals Not Found In a Slot ";
+                        return NotFound(new { Success, Message });
+                }
+            }
+            catch (Exception exception)
+            {
+            return BadRequest(new { Success = false, Message = exception.Message });
+            }
         }
 
         /// <summary>
@@ -392,28 +401,28 @@ namespace ParkingLotApi.Controllers
         [Route("Vehicals")]
         public IActionResult CheckAllVehical()
         {
-        try
-        {
-        var Response = this.parkingLotBL.CheckAllVehical();
+            try
+            {
+                var Response = this.parkingLotBL.CheckAllVehical();
 
-        if (Response != null)
-        {
-        bool Success = true;
-        var Message = "Vehical Found";
-        return Ok(new { Success , Message , Data = Response });
-        }
-        else
-        {
-        bool Success = false;
-        var Message = "Vehical Not Found";
-        return NotFound(new { Success , Message });
-        }
-        }
-        catch (Exception exception)
-        {
-        bool Success = false;
-        return BadRequest(new { Success , Message = exception.Message });
-        }
+                if (Response != null)
+                {
+                    bool Success = true;
+                    var Message = "Vehical Found";
+                    return Ok(new { Success , Message , Data = Response });
+                }
+                else
+                {
+                    bool Success = false;
+                    var Message = "Vehical Not Found";
+                    return NotFound(new { Success , Message });
+                }
+            }
+            catch (Exception exception)
+            {
+                bool Success = false;
+                return BadRequest(new { Success , Message = exception.Message });
+            }
         }
 
 
@@ -426,27 +435,27 @@ namespace ParkingLotApi.Controllers
         [Route("Vehicals/Park")]
         public IActionResult CheckParkVehical()
         {
-        try
-        {
-        var Response = this.parkingLotBL.CheckParkVehical();
+            try
+            {
+                    var Response = this.parkingLotBL.CheckParkVehical();
 
-        if (Response != null)
-        {
-        bool Success = true;
-        var Message = "Park Vehical Is Available";  
-        return Ok(new { Success, Message , Data = Response });
-        }
-        else
-        {
-        bool Success = false;
-        var Message = "Park Not Vehical Is Full";
-        return NotFound(new { Success , Message  });
-        }
-        }
-        catch (Exception exception)
-        {
-        return BadRequest(new { Success = false, Message = exception.Message });
-        }
+                if (Response != null)
+                {
+                        bool Success = true;
+                        var Message = "Park Vehical Is Available";  
+                        return Ok(new { Success, Message , Data = Response });
+                }
+                else
+                {
+                        bool Success = false;
+                        var Message = "Park Not Vehical Is Full";
+                        return NotFound(new { Success , Message  });
+                }
+            }
+            catch (Exception exception)
+            {
+                    return BadRequest(new { Success = false, Message = exception.Message });
+            }
         }
 
 
@@ -459,29 +468,27 @@ namespace ParkingLotApi.Controllers
         [Route("Vehicals/UnPark")]
         public IActionResult CheckUnParkVehical()
         {
-        try
-        {
-        var Response = this.parkingLotBL.CheckUnParkVehical();
+            try
+            {
+                    var Response = this.parkingLotBL.CheckUnParkVehical();
 
-        if (Response != null)
-        {
-        bool Success = true;
-        var Message = "UnPark Vehical Is Available";
-        return Ok(new { Success, Message , Data = Response });
+                if (Response != null)
+                {
+                        bool Success = true;
+                        var Message = "UnPark Vehical Is Available";
+                        return Ok(new { Success, Message , Data = Response });
+                }
+                else
+                {
+                        bool Success = false;
+                        var Message = "UnPark Not Vehical Is Full";
+                        return NotFound(new { Success , Message  });
+                }
+            }
+            catch (Exception exception)
+            {
+                    return BadRequest(new { Success = false, Message = exception.Message });
+            }
         }
-        else
-        {
-        bool Success = false;
-        var Message = "UnPark Not Vehical Is Full";
-        return NotFound(new { Success , Message  });
-        }
-        }
-        catch (Exception exception)
-        {
-        return BadRequest(new { Success = false, Message = exception.Message });
-        }
-        }
-
-
-}
+    }
 }
